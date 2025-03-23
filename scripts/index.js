964998d2e45637d7ffef83446f0a798e0c81de29
lennicat <eric.gore@outlook.com>
2025-03-23 04:09:42 -0400

    + new page for information regarding the source code for the website + added better directory, and file management for the website (allowing for cleaner and prettier urls when viewed on a browser) ~ general bug fixes and enhancements

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,11 @@
 const dialog = document.querySelector("dialog");
 const closeButton = document.querySelector("dialog button");
+
+const drawer = document.getElementById('drawer');
+const openDrawerButton = document.getElementById('open-drawer');
+const closeDrawerButton = document.getElementById('close-drawer');
+
+
 const musicPlayer = document.getElementById('player');
 let isMuted;
 window.onload = dialog.showModal();
@@ -28,3 +34,14 @@ muteButton.addEventListener("click", () => {
         icon.innerHTML = "volume_up"
     }
 });
+
+// Open the drawer when the button is clicked
+openDrawerButton.addEventListener('click', () => {
+    drawer.classList.add('open');
+    console.log("Test")
+  });
+  
+  // Close the drawer when the close button is clicked
+  closeDrawerButton.addEventListener('click', () => {
+    drawer.classList.remove('open');
+  });
